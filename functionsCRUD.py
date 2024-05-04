9b667e4ed521bade498e9709f4247880513d9a65
ademirMaltazard <ademir.mk98@gmail.com>
2024-05-04 18:22:15 -0300

    criação da função de DeleteProductByID e SearchAllproducts

--- functionsCRUD.py
+++ functionsCRUD.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def connectDB():
+def ConnectDB():
     conexao = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -13,15 +13,31 @@ def connectDB():
     return conexao, cursor
 
 
-def cadastrar(nome: str, preco: float, id: str, img: str):
+def RegisterNewProduct(nome: str, preco: float, id: str, img: str):
     ''' Faz a inserção de um produto no banco de dados. A imagem é a URL da imagem do produto na web.
     Utilizar formatos encurtados com quantidade de caracteres inferiores a 100
     Você poderá usar o https://www.encurtarlink.com/ para ajustar ao formato adequado'''
 
-    conexao, cursor = connectDB()
-    comando_sql = f"insert into produto (id_produto, nome_produto, preco_produto, image) value ('{id}', '{nome}',{preco},'{img}')"
+    conexao, cursor = ConnectDB()
+    query = f"insert into produto (id_produto, nome_produto, preco_produto, image) value ('{id}', '{nome}',{preco},'{img}')"
 
-    cursor.execute(comando_sql)
+    cursor.execute(query)
     conexao.commit()  # aqui é onde vamos inserir os dados
     print("Produto cadastrado com sucesso!")
     conexao.close()
+    
+def DeleteProductByID(id):
+    conexao, cursor = ConnectDB()
+    query = f"DELETE FROM produto WHERE (id_produto='{id}')"
+    cursor.execute(query)
+    conexao.commit()
+    conexao.close()
+    return print('Produto deletado com sucesso!')
+
+def SearchAllproducts():
+    conexao, cursor = ConnectDB()
+    query = f'SELECT * FROM produto'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
+
