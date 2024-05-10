4eb94c8fcc2c4620ac95fcedd5a166fa7248888c
ademirMaltazard <ademir.mk98@gmail.com>
2024-05-10 00:02:33 -0300

    Atualização massiva no código para uso de paginação com tabs

--- main.py
+++ main.py
@@ -3,63 +3,64 @@ import streamlit as st
 from functionsCRUD import *
 
 st.title('Sistema Dieguinho Alimentos - ME')
+pagina1, pagina2, pagina3, pagina4 = st.tabs(["Listar Produtos", "Cadastrar", "Atualizar", "Buscar Produto"])
 
-col1, col2 = st.columns(2)
-
-containerCadastrar = col1.container(border=True)
-containerAlterar = col2.container(border=True)
-containerList = st.expander('Mostrar todos os produtos')
-
-with containerCadastrar:
-    containerCadastrar.markdown('## Cadastro de produtos')
-
-    cod = containerCadastrar.text_input('Codigo do produto')
-    name = containerCadastrar.text_input('Nome do produto:', placeholder='Nome do produto com max de 100 caracteres')
-    price = float(containerCadastrar.number_input('Preço do produto:'))
-    image = containerCadastrar.text_input('URL da imagem do produto:', placeholder="URL com até 100 caracteres")
-
-    RegisterButton = containerCadastrar.button('CADASTRAR')
-
-    if RegisterButton:
-        RegisterNewProduct(name, price, cod, image)
-
-with containerAlterar:
-    containerAlterar.markdown('## Alterar produtos')
-    newCod = containerAlterar.text_input('Novo codigo do produto')
-    newName = containerAlterar.text_input('Novo nome do produto:', placeholder='Nome do produto com max de 100 caracteres')
-    newPrice = float(containerAlterar.number_input('Novo preço do produto:'))
-    newImage = containerAlterar.text_input('Nova URL da imagem do produto:', placeholder="URL com até 100 caracteres")
-
-    alterButton = containerAlterar.button('Alterar')
-
-with containerList:
+with pagina1:
     try:
         products = SearchAllproducts()
         productsTable = pd.DataFrame(products, columns=('id', 'nome', 'preço'))
         st.write(productsTable)
     except:
-        containerList.write('ERROR 404. Not found.')
-containerListOne = st.container(border=True)
+        pagina1.write('ERROR 404. Not found.')
 
-with containerListOne:
-    containerListOne.markdown("## Buscar um produto")
-    id = containerListOne.text_input('ID do produto para pesquisa')
-    SearchOneButton = containerListOne.button('Pesquisar')
+
+with pagina2:
+    pagina2.markdown('## Cadastro de produtos')
+
+    cod = pagina2.text_input('Codigo do produto')
+    name = pagina2.text_input('Nome do produto:', placeholder='Nome do produto com max de 100 caracteres')
+    price = float(pagina2.number_input('Preço do produto:'))
+    image = pagina2.text_input('URL da imagem do produto:', placeholder="URL com até 100 caracteres")
+
+    RegisterButton = pagina2.button('CADASTRAR')
+
+    if RegisterButton:
+        RegisterNewProduct(name, price, cod, image)
+
+with pagina3:
+    pagina3.markdown('## Alterar produtos')
+    newCod = pagina3.text_input('Novo codigo do produto')
+    newName = pagina3.text_input('Novo nome do produto:', placeholder='Nome do produto com max de 100 caracteres')
+    newPrice = float(pagina3.number_input('Novo preço do produto:'))
+    newImage = pagina3.text_input('Nova URL da imagem do produto:', placeholder="URL com até 100 caracteres")
+
+    alterButton = pagina3.button('Alterar')
+
+product = None
+with pagina4:
+    pagina4.markdown("## Buscar um produto")
+    id = pagina4.text_input('ID do produto para pesquisa')
+    SearchOneButton = pagina4.button('Pesquisar')
 
     if SearchOneButton:
         product = SearchOneproduct(id)
 
         if product:
-            subCol1, subCol2 = containerListOne.columns(2)
+            subCol1, subCol2 = pagina4.columns(2)
             subCol1.write(f'nome: {product[1]}')
             subCol2.write(f'Preço: {product[2]}')
-            st.write('Imagem:')
+            pagina4.write('Imagem:')
             try:
-                st.image(f'{product[3]}', width=200)
+                pagina4.image(f'{product[3]}', width=200)
             except:
-                st.write('Null')
-            subCol3, subCol4 = containerListOne.columns(2)
-            deleteButton = subCol4.button('Excluir')
+                pagina4.write('Null')
 
-            if deleteButton:
-                subCol3.write("Produto excluido com sucesso!")
+def delete():
+    result = DeleteProductByID(id)
+    if result == None:
+        result = "Produto não encontrado!!!"
+    pagina4.write(result)
+
+if product != None:
+    deleteButton = pagina4.button('Excluir produto', disabled=False, on_click=delete)
+    print(deleteButton)
